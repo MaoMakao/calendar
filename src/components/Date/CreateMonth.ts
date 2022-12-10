3274fa45acbe06c7f2e8907fe1dbfa5aa98c9aa1
max <evrei.bank@gmail.com>
2022-12-10 14:41:53 +0200

    fix renderDay

--- src/components/Date/CreateMonth.ts
+++ src/components/Date/CreateMonth.ts
@@ -14,18 +14,30 @@ const createMonth = ({
   const d = createDate({ date, locale });
   const { month: monthName, year, monthNumber, monthIndex } = d;
 
+  
+  const getDate = (dayNumber: number): Date => new Date(year, monthIndex, dayNumber)
+  
+  const getArrayOfDates = () => {
+    const days = [];
+    
+    for (let i = 0; i <= getMonthNumberOfDays(monthIndex, year) - 1; i += 1) {
+      days[i] = getDate(i + 1);
+    }
+    return days;
+  };
+
   const getDay = (dayNumber: number) =>
     createDate({
       date: new Date(year, monthIndex, dayNumber),
       locale,
     });
+
   const createMonthDays = () => {
     const days = [];
 
     for (let i = 0; i <= getMonthNumberOfDays(monthIndex, year) - 1; i += 1) {
       days[i] = getDay(i + 1);
     }
-
     return days;
   };
 
@@ -36,6 +48,7 @@ const createMonth = ({
     monthNumber,
     year,
     createMonthDays,
+    getArrayOfDates,
   };
 };
 
