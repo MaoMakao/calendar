741f234f5877edecfca47dc5f8a91ea98e03d974
Max <111875027+MaoMakao@users.noreply.github.com>
2023-01-19 22:34:55 +0300

    Revert "Cal 6"

--- db.js
+++ db.js
@@ -1,22 +1,5 @@
 module.exports = {
-  days: [
-    {
-      id: 234525245,
-
-      todos: [
+    todos: [
         { text: 'Hello', checked: false, id: 1323123, time: '00:00' },
-        { text: 'Hello', checked: false, id: 1323123, time: '00:00' },
-        { text: 'Hello', checked: false, id: 1323123, time: '00:00' },
-      ],
-    },
-    {
-      id: 111115,
-
-      todos: [
-        { text: 'Hello', checked: false, id: 131231223, time: '00:00' },
-        { text: 'Hello', checked: false, id: 1231233123, time: '00:00' },
-        
-      ],
-    },
-  ],
-};
+    ]
+}
